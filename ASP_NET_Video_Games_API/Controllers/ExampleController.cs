311d6b9525ed6ec463169f903fccb335fcb353d5
David La Grange <david@devcodecamp.com>
2022-03-29 08:25:00 -0500

    Added Examples Controller and Endpoints

---
+++ ASP_NET_Video_Games_API/Controllers/ExampleController.cs
@@ -0,0 +1,31 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ExampleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPublishers()
+        {
+            var gamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+            return Ok(gamePublishers);
+        }
+
+        [HttpGet("{pubName}")]
+        public IActionResult GetGamesByPublisher(string pubName)
+        {
+            var gamesByPublisher = _context.VideoGames.Where(vg => vg.Publisher == pubName);
+            return Ok(gamesByPublisher);
+        }
+    }
+}
